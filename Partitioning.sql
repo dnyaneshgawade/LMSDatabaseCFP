a152eab05305c9680b9a8b800af5072c9f65e024
dnyana <dnyaneshgawade903@gmail.com>
2021-09-28 22:55:34 +0530

    [Dnyanesh] Add : operations performed on LMSDatabase

--- Partitioning.sql
+++ Partitioning.sql
@@ -196,6 +196,8 @@ WHERE TABLE_SCHEMA = 'lmsdatabase' AND TABLE_NAME = 'fellowship_candidates_hash_
     doc_status varchar(20) DEFAULT NULL,
     remark varchar(150) DEFAULT NULL,
     creator_stamp datetime DEFAULT NULL,
+
+
     creator_user int(11) DEFAULT NULL,
     PRIMARY KEY (id)
  )
@@ -207,4 +209,85 @@ WHERE TABLE_SCHEMA = 'lmsdatabase' AND TABLE_NAME = 'fellowship_candidates_hash_
 SELECT TABLE_NAME, PARTITION_NAME, TABLE_ROWS, AVG_ROW_LENGTH, DATA_LENGTH
 FROM INFORMATION_SCHEMA.PARTITIONS
 WHERE TABLE_SCHEMA = 'lmsdatabase' AND TABLE_NAME = 'fellowship_candidates_key_partitioning';
+
+
+
+
+/* Range Column Partitioning*/
+CREATE TABLE fellowship_candidates_range_column_partitioning
+(
+    id int(11) NOT NULL,
+    first_name varchar(100) NOT NULL,
+    middle_name varchar(100) DEFAULT NULL,
+    last_name varchar(100) NOT NULL,
+    email varchar(50) NOT NULL,
+    mobile_num bigint(20) NOT NULL,
+    hired_city varchar(50) NOT NULL,
+    hired_date datetime NOT NULL,
+    degree varchar(50) NOT NULL,
+    aggr_per double DEFAULT NULL,
+    current_pincode int(11) DEFAULT NULL,
+    permanent_pincode int(11) DEFAULT NULL,
+    hired_lab varchar(20) DEFAULT NULL,
+    attitude_remark varchar(15) DEFAULT NULL,
+    communication_remark varchar(15) DEFAULT NULL,
+    knowledge_remark varchar(15) DEFAULT NULL,
+    birth_date date NOT NULL,
+    is_birth_date_verified int (1) DEFAULT 0,
+    parent_name varchar(50) DEFAULT NULL,
+    parent_occupation varchar(100) NOT NULL,
+    parent_mobile_num bigint(20) NOT NULL,
+    parent_annual_sal double DEFAULT NULL,
+    local_addr varchar(255) NOT NULL,
+    permanent_addr varchar(150) DEFAULT NULL,
+    photo_path varchar(500) DEFAULT NULL,
+    joining_date date DEFAULT NULL,
+    candidate_status varchar(20) NOT NULL,
+    personal_info_filled int (3) DEFAULT 0,
+    bank_info_filled int (3) DEFAULT 0,
+    educational_info_filled int (3) DEFAULT 0,
+    doc_status varchar(20) DEFAULT NULL,
+    remark varchar(150) DEFAULT NULL,
+    creator_stamp datetime DEFAULT NULL,
+    creator_user int(11) DEFAULT NULL
+ )
+PARTITION BY RANGE COLUMNS(id,parent_annual_sal,creator_user)
+(
+    PARTITION p0 VALUES LESS THAN (2,20000,2),
+    PARTITION p1 VALUES LESS THAN (4,30000,4),
+    PARTITION p2 VALUES LESS THAN (6,40000,6),
+    PARTITION p3 VALUES LESS THAN (10,50000,8)
+);
+
+
+CREATE TABLE employee_range_column_partitioning
+(
+    EmpId int,
+    FirstName varchar(50),
+    LastName varchar(50),
+    Age int,
+    Gender varchar(10),
+    Salary int(15)
+)
+PARTITION BY RANGE COLUMNS(EmpId,Age,Salary)
+(
+    PARTITION p0 VALUES LESS THAN (2,22,20000),
+    PARTITION p1 VALUES LESS THAN (4,24,30000),
+    PARTITION p3 VALUES LESS THAN (6,26,40000)
+);
+
+
+
+
+
+/*show partition*/
+
+SELECT TABLE_NAME, PARTITION_NAME, TABLE_ROWS, AVG_ROW_LENGTH, DATA_LENGTH
+FROM INFORMATION_SCHEMA.PARTITIONS
+WHERE TABLE_SCHEMA = 'lmsdatabase' AND TABLE_NAME = 'fellowship_candidates_range_column_partitioning';
+
+
+SELECT TABLE_NAME, PARTITION_NAME, TABLE_ROWS, AVG_ROW_LENGTH, DATA_LENGTH
+FROM INFORMATION_SCHEMA.PARTITIONS
+WHERE TABLE_SCHEMA = 'lmsdatabase' AND TABLE_NAME = 'employee_range_column_partitioning';
  
